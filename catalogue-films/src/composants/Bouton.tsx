bd3b416352d66cb2aaaf1a0990038800858b8a30
smarjane <marjane.selman@ynov.com>
2026-09-04 20:15:06 +0200

    question 2 termine

--- catalogue-films/src/composants/Bouton.tsx
+++ catalogue-films/src/composants/Bouton.tsx
@@ -1,5 +1,6 @@
 export type VarianteBouton = "primaire" | "secondaire" | "danger";
 
+
 export interface BoutonProps {
   libelle: string;
   variante?: VarianteBouton;   // "primaire" par défaut
@@ -7,11 +8,24 @@ export interface BoutonProps {
   onClick?: () => void;
 }
 
-function Bouton({libelle, variante = "primaire"} : BoutonProps){
+const COLORS: Record<VarianteBouton, string> = { //class T selon les variante
+  primaire: 'bg-green-500 text-white',
+  secondaire: 'bg-yellow-400 text-black',
+  danger: 'bg-red-500 text-white',
+};
+
+
+
+//appelle les classe selon les variante pr l 22
+function Bouton({libelle, variante = "primaire", desactive = false, onClick} : BoutonProps){ //recup les props et met valeur par defaut
+  return(
+    <button disabled={desactive} onClick={onClick} className={`px-4 py-2 rounded-lg font-medium focus:outline-none focus:ring-2 focus:ring-offset-2  ${COLORS[variante]}} 
+    `}>
+      {libelle}
+    </button>
+  );
 
 }
+export default Bouton
 
 
-<Bouton libelle="Valider" />
-<Bouton libelle="Supprimer" variante="danger" onClick={supprimer} />
-<Bouton libelle="Indisponible" desactive />
